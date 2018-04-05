49433bae806d70683277a1c7db1034cbcac64518
Ami Cooper and Kayl Eubanks <ami@>
2018-04-05 16:17:14 -0700

    fully functioning game of tic tac toe

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
-function Board (x,y,character){
-  this.x = x;
-  this.y = y;
-  this.character = character;
-}
-
-function Player(name, character){
-  this.name = name;
-  this.character = character;
-}
+// function Player (name,mark){
+//   this.name = name;
+//   this.mark = mark;
+// }
+// function Space (mark,x,y){
+//   this.x = x;
+//   this.y = y;
+//   this.mark = mark;
+// }
+// function Board (){
+//   this
+// }
+// function Board (x,y,character){
+//   this.x = x;
+//   this.y = y;
+//   this.character = character;
+// }
+//
+// function Player(name, character){
+//   this.name = name;
+//   this.character = character;
+// }
 
 
 var player1 = [];
@@ -27,46 +39,90 @@ $(document).ready(function(){
   $(".col-1").click(function(event){
     event.preventDefault();
     if (turnTracker % 2 === 0 || turnTracker === 0) {
-      var test = $(this).val();
-      debugger;
-      player1.push($(this).val());
+      var target = this.firstElementChild;
+      var spaceNumber = parseInt($(target).val());
+      player1.push(spaceNumber);
       $(this).text("X");
       turnTracker += 1;
-      console.log(player1);
     } else {
+      var target = this.firstElementChild;
+      var spaceNumber = parseInt($(target).val());
+      player2.push(spaceNumber);
       $(this).text("O");
       turnTracker += 1;
     }
-    if (turnTracker === 9) {
-      alert("Game Over");
+    if (player1.includes(1)===true && player1.includes(2)===true && player1.includes(3)===true ||  player1.includes(7)===true && player1.includes(8)===true && player1.includes(9)===true || player1.includes(2)===true && player1.includes(5)===true && player1.includes(8)===true || player1.includes(1)===true && player1.includes(5)===true && player1.includes(9)===true || player1.includes(4)===true && player1.includes(5)===true && player1.includes(6)===true || player1.includes(1)===true && player1.includes(4)===true && player1.includes(7)===true || player1.includes(3)===true && player1.includes(6)===true && player1.includes(9)===true || player1.includes(3)===true && player1.includes(5)===true && player1.includes(7)===true) {
+      $("div.winner1").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
+    } else if (player2.includes(1)===true && player2.includes(2)===true && player2.includes(3)===true ||  player2.includes(7)===true && player2.includes(8)===true && player2.includes(9)===true || player2.includes(2)===true && player2.includes(5)===true && player2.includes(8)===true || player2.includes(1)===true && player2.includes(5)===true && player2.includes(9)===true || player2.includes(4)===true && player2.includes(5)===true && player2.includes(6)===true || player2.includes(1)===true && player2.includes(4)===true && player2.includes(7)===true || player2.includes(3)===true && player2.includes(6)===true && player2.includes(9)===true || player2.includes(3)===true && player2.includes(5)===true && player2.includes(7)===true) {
+      $("div.winner2").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
+    } else if (turnTracker === 9) {
+      $("div.tie").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
     }
     $(this).addClass("unclickable");
   });
   $(".col-2").click(function(event){
     event.preventDefault();
     if (turnTracker % 2 === 0 || turnTracker === 0) {
+      var target = this.firstElementChild;
+      var spaceNumber = parseInt($(target).val());
+      player1.push(spaceNumber);
       $(this).text("X");
       turnTracker += 1;
     } else {
+      var target = this.firstElementChild;
+      var spaceNumber = parseInt($(target).val());
+      player2.push(spaceNumber);
       $(this).text("O");
       turnTracker += 1;
     }
-    if (turnTracker === 9) {
-      alert("Game Over");
+    if (player1.includes(1)===true && player1.includes(2)===true && player1.includes(3)===true ||  player1.includes(7)===true && player1.includes(8)===true && player1.includes(9)===true || player1.includes(2)===true && player1.includes(5)===true && player1.includes(8)===true || player1.includes(1)===true && player1.includes(5)===true && player1.includes(9)===true || player1.includes(4)===true && player1.includes(5)===true && player1.includes(6)===true || player1.includes(1)===true && player1.includes(4)===true && player1.includes(7)===true || player1.includes(3)===true && player1.includes(6)===true && player1.includes(9)===true || player1.includes(3)===true && player1.includes(5)===true && player1.includes(7)===true) {
+      $("div.winner1").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
+    } else if (player2.includes(1)===true && player2.includes(2)===true && player2.includes(3)===true ||  player2.includes(7)===true && player2.includes(8)===true && player2.includes(9)===true || player2.includes(2)===true && player2.includes(5)===true && player2.includes(8)===true || player2.includes(1)===true && player2.includes(5)===true && player2.includes(9)===true || player2.includes(4)===true && player2.includes(5)===true && player2.includes(6)===true || player2.includes(1)===true && player2.includes(4)===true && player2.includes(7)===true || player2.includes(3)===true && player2.includes(6)===true && player2.includes(9)===true || player2.includes(3)===true && player2.includes(5)===true && player2.includes(7)===true) {
+      $("div.winner2").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
+    } else if (turnTracker === 9) {
+      $("div.tie").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
     }
     $(this).addClass("unclickable");
   });
   $(".col-3").click(function(event){
     event.preventDefault();
     if (turnTracker % 2 === 0 || turnTracker === 0) {
+      var target = this.firstElementChild;
+      var spaceNumber = parseInt($(target).val());
+      player1.push(spaceNumber);
       $(this).text("X");
       turnTracker += 1;
     } else {
+      var target = this.firstElementChild;
+      var spaceNumber = parseInt($(target).val());
+      player2.push(spaceNumber);
       $(this).text("O");
       turnTracker += 1;
     }
-    if (turnTracker === 9) {
-      alert("Game Over");
+    if (player1.includes(1)===true && player1.includes(2)===true && player1.includes(3)===true ||  player1.includes(7)===true && player1.includes(8)===true && player1.includes(9)===true || player1.includes(2)===true && player1.includes(5)===true && player1.includes(8)===true || player1.includes(1)===true && player1.includes(5)===true && player1.includes(9)===true || player1.includes(4)===true && player1.includes(5)===true && player1.includes(6)===true || player1.includes(1)===true && player1.includes(4)===true && player1.includes(7)===true || player1.includes(3)===true && player1.includes(6)===true && player1.includes(9)===true || player1.includes(3)===true && player1.includes(5)===true && player1.includes(7)===true) {
+      $("div.winner1").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
+    } else if (player2.includes(1)===true && player2.includes(2)===true && player2.includes(3)===true ||  player2.includes(7)===true && player2.includes(8)===true && player2.includes(9)===true || player2.includes(2)===true && player2.includes(5)===true && player2.includes(8)===true || player2.includes(1)===true && player2.includes(5)===true && player2.includes(9)===true || player2.includes(4)===true && player2.includes(5)===true && player2.includes(6)===true || player2.includes(1)===true && player2.includes(4)===true && player2.includes(7)===true || player2.includes(3)===true && player2.includes(6)===true && player2.includes(9)===true || player2.includes(3)===true && player2.includes(5)===true && player2.includes(7)===true) {
+      $("div.winner2").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
+    } else if (turnTracker === 9) {
+      $("div.tie").show();
+      $("div.row").addClass("unclickable");
+      $("button.refresh").show();
     }
     $(this).addClass("unclickable");
   });
